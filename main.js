eb2fc6451c5f6418230500414423153401cde62c
chenshane911 <shane175911@yahoo.com.tw>
2016-06-22 21:07:50 +0800

    new section

--- main.js
+++ main.js
@@ -5,3 +5,31 @@ burger.addEventListener("click", (e) => {
   document.body.classList.toggle("open");
   burger.classList.toggle("open");
 });
+
+
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+//An image to overlay
+$overlay.append($image);
+
+//Add overlay
+$("body").append($overlay);
+
+//click the image and a scaled version of the full size image will appear
+$(".photo-gallery a").on('click', function(event) {
+    event.preventDefault();
+    var imageLocation = $(this).attr("href");
+
+    //update overlay with the image linked in the link
+    $image.attr("src", imageLocation);
+
+    //show the overlay
+    $overlay.addClass('show');
+});
+
+$("#overlay").on('click', function() {
+    $( "#overlay" ).removeClass('show');
+});
+
